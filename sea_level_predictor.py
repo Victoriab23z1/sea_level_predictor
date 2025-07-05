c1c05d6617218fa7667e200513939e6769e11d64
Victoriab23z1 <165341608+Victoriab23z1@users.noreply.github.com>
2025-07-04 23:22:09 -0400

    Create sea_level_predictor.py

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,1 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def draw_plot():
+    df = pd.read_csv('epa-sea-level.csv')
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], label='Datos reales', color='blue')
+
+    res = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x_pred = pd.Series(range(1880, 2051))
+    y_pred = res.slope * x_pred + res.intercept
+    plt.plot(x_pred, y_pred, 'r', label='Tendencia desde 1880')
+
+    df_recent = df[df['Year'] >= 2000]
+    res_recent = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
+    x_recent = pd.Series(range(2000, 2051))
+    y_recent = res_recent.slope * x_recent + res_recent.intercept
+    plt.plot(x_recent, y_recent, 'green', label='Tendencia desde 2000')
+
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    plt.legend()
+    plt.grid(True)
+
+    plt.savefig('sea_level_plot.png')
+    return plt.gca()
 
